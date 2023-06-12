537838eb10094ee54da7bd482c9d475776346b39
Ljuzifer <121060706+Ljuzifer@users.noreply.github.com>
2023-06-12 23:07:18 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,18 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const imagesMarkup = galleryItems.reduce(
+  (acc, { original, preview, description }) =>
+    (acc += `<li>
+  <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>`),
+  ''
+);
+
+galleryOfImages.insertAdjacentHTML('beforeend', imagesMarkup);
